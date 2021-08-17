47238a962da1007b64c02a5723bce8c19e3ec05b
daiki <daiki.1993.0807@gmail.com>
2021-08-18 06:20:31 +0900

    Improve the appearance of code.

--- src/train_network.py
+++ src/train_network.py
@@ -29,7 +29,7 @@ def train_network():
 
     a, b, c = DN_INPUT_SHAPE
     xs = np.array(xs)
-    xs = xs.reshape(len(xs), c, a, b).transpose(0, 2, 3, 1)
+    xs = xs.reshape(len(xs), c, a, b).transpose(0, 2, 3, 1)   
     xs = xs/INITIAL_LIFE
     y_policies = np.array(y_policies)
     y_values = np.array(y_values)
