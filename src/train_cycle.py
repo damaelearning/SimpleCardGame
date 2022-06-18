06f58f6bc023997a8304c884bdafcbef4eed24bb
daiki <daiki.1993.0807@gmail.com>
2022-06-18 22:57:23 +0900

    Change the evaluation to update model

--- src/train_cycle.py
+++ src/train_cycle.py
@@ -1,7 +1,7 @@
 from dual_network import dual_network
 from self_play import multiProcessSelfPlay
 from train_network import train_network
-from evaluate_network import multi_process_evaluate_network
+from evaluate_network import multi_process_evaluate_network, update_best_player
 from evaluate_best_player import multi_process_evaluate_best_player
 
 if __name__ == '__main__':
@@ -14,7 +14,8 @@ if __name__ == '__main__':
 
         train_network()
 
-        update_best_player = multi_process_evaluate_network(5)
+        #update_best_player = multi_process_evaluate_network(5)
+        update_best_player()
 
-        if update_best_player:
-            multi_process_evaluate_best_player(5)
+        #if update_best_player:
+        multi_process_evaluate_best_player(5)
