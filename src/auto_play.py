b8e18abc79de3867e6d197ee2cd17a6c651849df
damaelearning <daiki.1993.0807@gmail.com>
2022-09-04 12:00:00 +0900

    Integrate methods, "make_log" and "calc_win_rate" to AutoPlay class. When executing "self play", "make_log" shall be called. In the case of "evaluate ~", "calc_win_rate" shall be called.

--- src/auto_play.py
+++ src/auto_play.py
@@ -1,5 +1,4 @@
 from faulthandler import cancel_dump_traceback_later
-import imp
 import tensorflow as tf
 import time
 from game import INITIAL_LIFE, State, Actor, FIELDS_NUM, HANDS_NUM
@@ -11,12 +10,12 @@ from tensorflow.keras import backend as K
 from pathlib import Path
 import  numpy as np
 import pickle
-import os
 import multiprocessing
 import concurrent.futures as futures
 from const import MODEL_DIR, HISTORY_DIR
 import platform
 from functools import partial
+import os
 
 from search import mcts_action, ismcts_action
 
@@ -39,9 +38,9 @@ class AutoPlay:
         self.action2 = action
         self.model_path2 = model_path
     
-    @staticmethod
-    def play(action_names, model_paths, temperature, logging=False):
-        actions = AutoPlay.get_actions(action_names, temperature, model_paths)
+    @classmethod
+    def play(cls, action_names, model_paths, temperature, logging=False):
+        actions = cls.get_actions(action_names, temperature, model_paths)
         # for logging
         history = []
         height, width, channel = DN_INPUT_SHAPE
@@ -90,24 +89,27 @@ class AutoPlay:
                 next_actions.append(next_action_by(search=pv_ismcts, model=model, temperature=temperature))
         return next_actions
 
-    def make_play_log(self, process_num, game_count, temperature, label="Completed"):
+    def multi_play(self, process_num, game_count, action_names, model_paths, 
+                        temperature, logging=False, label="Completed"):
+        count = 0
+        value = 0
+        historys = []
         canceled = False
-        action_names = (self.action1, self.action2)
-        model_paths = (self.model_path1, self.model_path2)
         with futures.ProcessPoolExecutor(max_workers=process_num) as executor:
-            results = [executor.submit(AutoPlay.play, 
+            results = [executor.submit(self.__class__.play, 
                     action_names=action_names, temperature= temperature, 
-                    model_paths=model_paths, logging = True) 
+                    model_paths=model_paths, logging = logging) 
                     for _ in range(game_count)]
-            count = 0
-            historys = []
             try:
                 for result in futures.as_completed(results):
-                    _, history = result.result()
-                    historys.extend(history)
+                    value, history = result.result()
+                    if logging:
+                        historys.extend(history)
+                    
+                    value += value
 
                     count += 1
-                    print('\r{}} {}/{}'.format(label, count, game_count), end='')
+                    print('\r{} {}/{}'.format(label, count, game_count), end='')
             except KeyboardInterrupt:
                 for future in results:
                     future.cancel()
@@ -115,7 +117,25 @@ class AutoPlay:
                 for process in executor._processes.values():
                     process.kill()
 
-        if not canceled : write_data(historys)
+        if not canceled and logging: write_data(historys)
+        return value
+    
+    def make_play_log(self, process_num, game_count, temperature=1.0, 
+                        process_name="Make play log"):
+        print(process_name)
+        action_names = (self.action1, self.action2)
+        model_paths = (self.model_path1, self.model_path2)
+        self.multi_play(process_num, game_count, action_names, model_paths, temperature, logging=True)
+
+    def calc_win_rate(self, process_num, game_count, temperature=0.0, 
+                        process_name=None):
+        if process_name == None:
+            process_name = "Calc win rate {} VS {}".format(self.action1, self.action2)
+        print(process_name)
+        action_names = (self.action1, self.action2)
+        model_paths = (self.model_path1, self.model_path2)
+        value = self.multi_play(process_num, game_count, action_names, model_paths, temperature)
+        print("Win rate : {}".format(value/game_count))
 
 
 def first_player_value(ended_state):
@@ -133,81 +153,8 @@ def write_data(historys):
     with open(path, mode='wb') as f:
         pickle.dump(historys, f)
 
-def play(next_actions, logging=False):
-    # for logging
-    history = []
-    height, width, channel = DN_INPUT_SHAPE
-
-    # initialize game state
-    first_player = Actor(is_first_player=True)
-    second_player = Actor(is_first_player=False)
-    state = State(first_player, second_player)
-    state = state.game_start()
-
-    while True:
-        if state.is_done():
-            break
-        state = state.start_turn() if state.is_starting_turn else state
-        if state.is_done():
-            break
-
-        next_action = next_actions[0] if state.turn_owner.is_first_player else next_actions[1]
-        action, policy = next_action(state)
-
-        if logging:
-            input = convert_state_to_input(state, height, width, channel)
-            history.append([input, policy, state.turn_owner.is_first_player])
-
-        state = state.next(action)
-    
-    value = first_player_value(state)
-    if logging:
-        for i in range(len(history)):
-            history[i][2] = value if history[i][2] else -value
-    return value, history
-
-def play_with_action_name(action_names, temperature, logging=False):
-    next_actions = []
-    for action in action_names:
-        if action == "mcts":
-            next_actions.append(mcts_action)
-        if action == "ismcts":
-            next_actions.append(ismcts_action)
-        if action == "pv_mcts": 
-            next_actions.append(next_action_by(search=pv_mcts, model=model, temperature=temperature))
-        if action == "pv_ismcts":
-            next_actions.append(next_action_by(search=pv_ismcts, model=model, temperature=temperature))
-    return play(next_actions, logging)
-
-
-def self_play(process_num, game_count):
-    canceled = False
-    action_names = ("pv_mcts", "pv_mcts")
-    with futures.ProcessPoolExecutor(max_workers=process_num) as executor:
-        results = [executor.submit(play_with_action_name, action_names=action_names, temperature= 1.0, logging = True) for _ in range(game_count)]
-        count = 0
-        historys = []
-        try:
-            for result in futures.as_completed(results):
-                _, history = result.result()
-                historys.extend(history)
-
-                count += 1
-                print('\rSelfPlay {}/{}'.format(count, SP_GAME_COUNT), end='')
-        except KeyboardInterrupt:
-            for future in results:
-                cancel = future.cancel()
-                print(cancel)
-            canceled = True
-            for process in executor._processes.values():
-                process.kill()
-
-    if not canceled : write_data(historys)
-    print("finished")
-
 if __name__ == '__main__':
     auto_play = AutoPlay()
     auto_play.set_action1("pv_mcts", MODEL_DIR/'best.h5')
     auto_play.set_action2("pv_mcts", MODEL_DIR/'best.h5')
-    auto_play.make_play_log(3, 1000, 1)
-    # self_play(3, 1000)
+    auto_play.calc_win_rate(3, 1000, 1)
